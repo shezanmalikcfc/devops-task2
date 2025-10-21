1e9b4273c37ce722386008579b97bb51478d8a38
shezanmalikcfc <shezan.malik2@gmail.com>
2025-10-21 19:53:34 +0530

    DevOps Task 2: minimal CI/CD pipeline

--- app.js
+++
@@ -1,9 +0,0 @@
-@'
-const http = require('http');
-const PORT = process.env.PORT || 8080;
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type':'text/plain'});
-  res.end('Hello from Jenkins CI/CD!\n');
-});
-server.listen(PORT, () => console.log('Server running on port ' + PORT));
-'@ | Set-Content -Path app.js
